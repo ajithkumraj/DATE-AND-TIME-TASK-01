e37e620e894101c4b4717ea7637be3e1c8fa3063
ajithkumraj <ajithkumarajak98@gmail.com>
2022-02-25 10:28:42 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,15 @@ import { formatDate } from '@angular/common';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-   date = formatDate(new Date(), 'dd/MM/yyyy', 'en-US');
-Date: Date;
+export class AppComponent {
+  date = formatDate(new Date(), 'yyyy-MM-dd', 'en-US');
+  Date: Date;
 
-constructor (){
-  setInterval(()=>{
-    this.Date = new Date();
-  }, 1000);
-}
+  constructor() {
+    setInterval(() => {
+      this.Date = new Date();
+    }, 1000);
+  }
 }
